f4c03fd364cb65e439841357db15af28e9a4d0b3
Robin Naundorf <r.naundorf@fh-muenster.de>
2016-12-01 08:52:12 +0100

    Add Models to admin site (#14)

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ship, Tour, Quota, Agent, Customer, Reservation
 
-# Register your models here.
+admin.site.register(Ship)
+admin.site.register(Tour)
+admin.site.register(Quota)
+admin.site.register(Agent)
+admin.site.register(Customer)
+admin.site.register(Reservation)
