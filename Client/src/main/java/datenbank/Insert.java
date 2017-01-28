5b419123ba8cc8340d12c07778ddc318775e48fa
Jonas Droste <jonas.droste@jonas-droste.com>
2017-01-28 16:19:38 +0100

    Create insert methods to build up local cache

--- Client/src/main/java/datenbank/Insert.java
+++ Client/src/main/java/datenbank/Insert.java
@@ -8,10 +8,18 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import api_client.agent;
+import api_client.customer;
+import api_client.quota;
+import api_client.reservation;
+import api_client.ship;
+import api_client.tour;
+
+
 /**
  * Hier werden die Datensätze in die Datenbanken hinzugefügt.
  * 
- * @author AgentFriz
+ * @author AgentFriz, Jonas Droste
  *
  */
 public class Insert {
@@ -19,7 +27,7 @@ public class Insert {
 	private Connection con = null;
 	private PreparedStatement pst = null;
 
-	private String url = "jdbc:postgresql://localhost:5432/piratebaytours";
+	private String url = "jdbc:sqlite:" + "db.sqlite3";
 	private String user = "postgres";
 	private String password = "1q1q1q1q";
 
@@ -59,4 +67,164 @@ public class Insert {
 			}
 		}
 	}
+
+	public void insertAgent(agent tmp){
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+
+			String stm = "INSERT INTO agents(id, name) VALUES( "+ tmp.id + ", '" + tmp.name + "')";
+			System.out.println(stm);
+			pst = con.prepareStatement(stm);
+			pst.executeUpdate();
+
+		} catch (SQLException ex) {
+			Logger lgr = Logger.getLogger(Insert.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+		} finally {
+
+			try {
+				if (pst != null) {
+					pst.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+				Logger lgr = Logger.getLogger(Insert.class.getName());
+				lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			}
+		}
+	}
+
+	public void insertCustomer(customer tmp){
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+
+			String stm = "INSERT INTO customers(id, name) VALUES( "+ tmp.id + ", '" + tmp.name + "')";
+			System.out.println(stm);
+			pst = con.prepareStatement(stm);
+			pst.executeUpdate();
+
+		} catch (SQLException ex) {
+			Logger lgr = Logger.getLogger(Insert.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+		} finally {
+
+			try {
+				if (pst != null) {
+					pst.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+				Logger lgr = Logger.getLogger(Insert.class.getName());
+				lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			}
+		}
+	}
+
+
+	public void insertQuota(quota tmp){
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+
+			String stm = "INSERT INTO quotas(count, tour, agent) VALUES( "+ tmp.count + ", " + tmp.tour + ", " + tmp.agent + ")";
+			System.out.println(stm);
+			pst = con.prepareStatement(stm);
+			pst.executeUpdate();
+
+		} catch (SQLException ex) {
+			Logger lgr = Logger.getLogger(Insert.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+		} finally {
+
+			try {
+				if (pst != null) {
+					pst.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+				Logger lgr = Logger.getLogger(Insert.class.getName());
+				lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			}
+		}
+	}
+
+	public void insertShip(ship tmp){
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+
+			String stm = "INSERT INTO ships(id, name, seats) VALUES( "+ tmp.id + ", '" + tmp.name + "', " + tmp.seats + ")";
+			System.out.println(stm);
+			pst = con.prepareStatement(stm);
+			pst.executeUpdate();
+
+		} catch (SQLException ex) {
+			Logger lgr = Logger.getLogger(Insert.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+		} finally {
+
+			try {
+				if (pst != null) {
+					pst.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+				Logger lgr = Logger.getLogger(Insert.class.getName());
+				lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			}
+		}
+	}
+
+
+	public void insertTour(tour tmp){
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+
+			String stm = "INSERT INTO tours(id, name, date, time, ship) VALUES( "+ tmp.id + ", '" + tmp.name + "', '" + tmp.date+"', '" + tmp.time+"', " + tmp.id + ")";
+			System.out.println(stm);
+			pst = con.prepareStatement(stm);
+			pst.executeUpdate();
+
+		} catch (SQLException ex) {
+			Logger lgr = Logger.getLogger(Insert.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+		} finally {
+
+			try {
+				if (pst != null) {
+					pst.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+				Logger lgr = Logger.getLogger(Insert.class.getName());
+				lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			}
+		}
+	}
+
+
+
 }
