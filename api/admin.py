e01b9fcb102d15f57d845ac517132d3ad96dc4a1
Jonas Droste <jonas.droste@jonas-droste.com>
2017-03-02 18:34:40 +0100

    clean structure

--- api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Ship, Tour, Quota, Agent, Customer, Reservation
-
-admin.site.register(Ship)
-admin.site.register(Tour)
-admin.site.register(Quota)
-admin.site.register(Agent)
-admin.site.register(Customer)
-admin.site.register(Reservation)
